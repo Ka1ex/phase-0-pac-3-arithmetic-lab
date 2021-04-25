e83cf0fdb5e06147bdad9f5c85a6c90fa22596dc
ka1ex <kalexadamson@hotmail.com>
2021-04-25 11:39:41 -0500

    Completed Assingment

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a, b){
+    return a + b
+}
+function subtract(a, b){
+    return a - b
+}
+function multiply(a, b){
+    return a * b
+}
+function divide(a, b){
+    return a / b
+}
+function increment(a){
+    return a += 1
+}
+function decrement(a){
+    return a -= 1
+}
+function makeInt(n){
+    let newInt = parseInt(n, 10)
+
+    return newInt
+}
+function preserveDecimal(given){
+    let givenDecimal = parseFloat(given)
+    return givenDecimal
+}
